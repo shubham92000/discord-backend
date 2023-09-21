a4a03d7217faa28b6ce4da3eb9ae4af153dc543e
shubham92000 <sbhsingh616@gmail.com>
2023-09-21 11:19:07 +0530

    init commit

---
+++ src/main/java/com/example/discordBackend/DiscordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.discordBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discordBackend/DiscordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discordBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
